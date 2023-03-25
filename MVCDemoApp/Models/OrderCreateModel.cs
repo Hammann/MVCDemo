aa375457da0a2ebd388284f1723ba9e1f13bcebe
Hammann <brandenhammann@gmail.com>
2023-03-25 18:30:29 -0500

    Add project files.

---
+++ MVCDemoApp/Models/OrderCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MVCDemoApp.Models
+{
+    public class OrderCreateModel
+    {
+        public OrderModel Order { get; set; } = new OrderModel();
+        public List<SelectListItem> FoodItems { get; set; } = new List<SelectListItem>();
+    }
+}
